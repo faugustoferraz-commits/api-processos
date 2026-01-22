568e1b377d717f3ead6eb3f42d3cf971c32d272b
faugustoferraz-commits <faugustoferraz@gmail.com>
2026-01-22 00:17:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+import express from "express";
+import { chromium } from "playwright";
+
+const app = express();
+app.use(express.json());
+
+app.post("/buscar", async (req, res) => {
+  try {
+    const { link } = req.body;
+
+    const browser = await chromium.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto(link, { waitUntil: "networkidle" });
+
+    const resultado = await page.content();
+
+    await browser.close();
+    res.json({ ok: true, html: resultado });
+
+  } catch (e) {
+    res.json({ ok: false, erro: e.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
 
